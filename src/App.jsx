a1385cf1445fbced4abfa0ec00b988175cc32e5b
MWandall <144394104+MWandall@users.noreply.github.com>
2023-11-26 20:36:38 -0600

    refactored for react router. also, added bootsrtap to use as far as preferance per component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import ProfilePicture from './components/ProfilePicture'
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
 
 function App() {
-  
-
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-<Header />
-<div className=' hidden md:block'>
-<ProfilePicture />
-</div>
-</>
-  )
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
